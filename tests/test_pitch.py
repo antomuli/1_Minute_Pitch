b25d3f80817c8413eeac0f800aeee903509385ca
Anthony Muli <mulianthony561@gmail.com>
2020-02-17 15:14:09 +0300

    Added tests for pitches

---
+++ tests/test_pitch.py
@@ -0,0 +1,27 @@
+from app.models import Comment,User,Pitch
+from app import db
+import unittest
+
+class PitchModelTest(unittest.TestCase):
+    def setUp(self):
+        self.user_James = User(username = 'James',password = '12345', email = 'james@ms.com')
+        self.new_pitch = Pitch(id=1,pitch_title='Test',pitch_content='This is a test pitch',category="interview",user = self.user_James,likes=0,dislikes=0)
+
+    def tearDown(self):
+        Pitch.query.delete()
+        User.query.delete()
+
+    def test_check_instance_variables(self):
+        self.assertEquals(self.new_pitch.pitch_title,'Test')
+        self.assertEquals(self.new_pitch.pitch_content,'This is a test pitch')
+        self.assertEquals(self.new_pitch.category,"interview")
+        self.assertEquals(self.new_pitch.user,self.user_James)
+
+    def test_save_pitch(self):
+        self.new_pitch.save_pitch()
+        self.assertTrue(len(Pitch.query.all())>0)
+
+    def test_get_pitch_by_id(self):
+        self.new_pitch.save_pitch()
+        got_pitch = Pitch.get_pitch(1)
+        self.assertTrue(got_pitch is not None)
